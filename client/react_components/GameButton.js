91529efae9b26b58a9d41aeb2b9816273e4b4faa
Asher Israel <artificialisrael@yahoo.com>
2016-05-20 11:16:04 -0700

    random bord with accurate activity logging

--- client/react_components/GameButton.js
+++ client/react_components/GameButton.js
@@ -3,6 +3,7 @@ import React from "react";
 export default class GameButton extends React.Component{
 	constructor(props){
 		super(props);
+		this.setState = this.setState.bind(this);
 		this.state = {
 			color: props.color,
 			width: props.width,
@@ -10,9 +11,26 @@ export default class GameButton extends React.Component{
 			delay: props.delay
 		}
 	}
+	componentWillReceiveProps(nextProps){
+		this.setState({color: nextProps.color, width: nextProps.width, height: nextProps.height})
+	}
+	handleClick(e){
+		console.log(e.pageX);
+		var data = {
+			color: this.state.color,
+			width: this.state.width,
+			height: this.state.height,
+			x: e.pageX,
+			y: e.pageY
+		}
+		this.props.recordAct(data);
+	}
 	render(){
+		var thisClass = "grid-item "+this.state.width+" "+this.state.height;
 		return(
-			<button style={{backgroundColor: this.state.color}} onClick={this.props.recordAct(this.state)}>test</button>
+			<div className={thisClass}>
+				<button className="gameButton" type="button" style={{backgroundColor: this.state.color}} onClick={(e) => this.handleClick(e)}></button>
+			</div>
 		)
 	}
 }
--- client/react_components/GameController.js
+++ client/react_components/GameController.js
@@ -7,28 +7,110 @@ export default class GameController extends React.Component{
 		console.log("Game loaded");
 		this.setState = this.setState.bind(this);
 		this.record = this.record.bind(this);
+		this.randomWidth = this.randomWidth.bind(this);
+		this.randomHeight = this.randomHeight.bind(this);
+		this.randomColor = this.randomColor.bind(this);
+		this.makeBoard = this.makeBoard.bind(this);
 		this.state = {
 			socket: props.route.socket,
 			username: null,
-			activities: []
+			activities: [],
+			board: []
 		}
+	}
+	componentWillMount(){
+		this.makeBoard();
 	}
 	componentDidMount(){
 		var that = this;
 		this.state.socket.on("logged_in", function(data){
 			that.setState({username: data.name});
 		})
+		var elem = document.querySelector('.grid');
+		var msnry = new Masonry( elem, {
+		  itemSelector: '.grid-item',
+		  columnWidth: ".grid-sizer",
+		  percentPosition: true
+		});
+	}
+	componentDidUpdate(nextState){
+		var elem = document.querySelector('.grid');
+		var msnry = new Masonry( elem, {
+		  itemSelector: '.grid-item',
+		  columnWidth: ".grid-sizer",
+		  percentPosition: true
+		});
+	}
+	makeBoard(){
+		var board = [];
+		for(var i=0;i<20;i++){
+			board.push(<GameButton key={i} color={this.randomColor()} width={this.randomWidth()} height={this.randomHeight()} recordAct={this.record}/>)
+		}
+		this.setState({board: board})
+	}
+	randomHeight(){
+		let picker = Math.floor(Math.random()*(6-1))+1;
+		switch(picker){
+			case 1:
+				return "";
+				break;
+			case 2:
+				return "grid-item--height2";
+				break;
+			case 3:
+				return "grid-item--height3";
+				break;
+			default:
+				return "";
+				break;
+		}
+	}
+	randomWidth(){
+		let picker = Math.floor(Math.random()*(6-1))+1;
+		switch(picker){
+			case 1:
+				return "";
+				break;
+			case 2:
+				return "grid-item--width2";
+				break;
+			case 3:
+				return "grid-item--width3";
+				break;
+			default:
+				return "";
+				break;
+		}
+	}
+	randomColor(){
+		return 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
 	}
 	record(buttonPressed){
+
+		console.log(buttonPressed)
 		var activities = this.state.activities.slice();
 		activities.push(buttonPressed);
 		this.setState({activities: activities});
 	}
+	reset(){
+		this.setState({board: []});
+		this.makeBoard();
+	}
 	render(){
 		console.log("Game rendered");
 		return(
-			<div className="container">
-				<h1>{this.state.username}</h1>
+			<div className="container-fluid">
+				<div className="row">
+					<h1 className="col-md-1">{this.state.username}</h1>
+					<div className="col-md-10"></div>
+					<div className="col-md-1"><button className="btn" type="button" onClick={() => this.reset()}>Reset</button></div>
+				</div>
+				<div className="row board">
+					<div className="gameBoard grid">
+						<div className="grid-sizer"></div>
+						{this.state.board}
+					</div>
+				</div>
 			</div>
 		)
 	}
